10307385ad4a3f16c118d55e341c828ba5a469c4
Eoghan Barker <eoghanbarker01@gmail.com>
2023-11-03 13:31:29 +0000

    Process the image directly in the POST endpoint

--- src/flask-server.py
+++ src/flask-server.py
@@ -1,6 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
+import os
+from werkzeug.utils import secure_filename
+from .FoodRecognition import IdentifyFood
 
 app = Flask(__name__)
 CORS(app)
@@ -10,12 +13,29 @@ CORS(app)
 def upload():
     # Get the file from post request
     file = request.files['file']
-    file_contents = file.read()
+    # file_contents = file.read()
+    
 
-    # TODO: Process the image here (e.g. apply ML model) and return the result
+    # TODO: Process the image asynchronously using a message queue
+    # For now just call the function directly
+    if file:
+        # Secure the filename to avoid unsafe characters
+        filename = secure_filename(file.filename)
+        # Save the file to a temporary location to be processed
+        temp_path = os.path.join('/path/to/temp', filename)
+        file.save(temp_path)
+
+        # Call the function to process the image
+        result = IdentifyFood(temp_path)
+
+        # Optionally remove the temporary file if no longer needed
+        os.remove(temp_path)
+        
+        return jsonify(result=result)
 
     # Return the result
-    if file_contents is None:
+    # if file_contents is None:
+    if file.filename == '':
         response = {'status': 'error', 'message': 'Error processing image'}
     else:
         response = {'status': 'success', 'message': 'File uploaded successfully'}
