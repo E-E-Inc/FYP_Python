47b10334bb55c3fca406d4591744edc3bb75d52f
emmacelia <emmagarvey345@gmail.com>
2023-11-08 14:28:42 +0000

    Fix to only detect food

--- src/FoodRecognition/IdentifyFoodYolo.py
+++ src/FoodRecognition/IdentifyFoodYolo.py
@@ -10,16 +10,27 @@ def Identification(image_path):
     load_image = cv2.imread(image_path)
 
     # Run the model on the image and store the results
-    result = model(load_image)
+    result = model(load_image, classes=[46])
 
     #Store the class id of the first detected object 
-    detected_class_id = result[0].pred[0]
+    detected_class_id = result[0].boxes.cls.item()
+
 
     #List of Ultralytics classes
     ultralytics_classes = {
-      20: 'banana', 21: 'apple', 22: 'sandwich', 23: 'orange', 24: 'broccoli', 25: 'carrot', 26: 'hot dog',
-        27: 'pizza', 28: 'donut', 29: 'cake'
-
+     0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train',
+        7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter',
+        13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant',
+        21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie',
+        28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite',
+        34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket',
+        39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl',
+        46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog',
+        53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed',
+        60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard',
+        67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator',
+        73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier',
+        79: 'toothbrush'
     }
 
     #If the item name is in the class
@@ -33,10 +44,16 @@ def Identification(image_path):
     s = detected_item
     return s
 
+#Checks if script is being run directly
 if __name__ == '__main__':
+    #Sets up command line argument parser for the script and provide a description 
     parser = argparse.ArgumentParser(description='Process an image.')
+
+    #Specifies that image_path must be a string
     parser.add_argument('image_path', type=str, help='The path to the image file')
 
+    #Parses the arguement when the script is run
     args = parser.parse_args()
-
+    
+    #Calls Identification function with the argument image_path
     Identification(args.image_path)
