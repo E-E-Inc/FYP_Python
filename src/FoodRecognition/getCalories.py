c09245d0f2ba662058e9521c4f360853c900c569
emmacelia <emmagarvey345@gmail.com>
2024-02-08 09:41:07 +0000

    Update calories based on portion size

--- src/FoodRecognition/getCalories.py
+++ src/FoodRecognition/getCalories.py
@@ -1,13 +1,31 @@
 import requests
 from testing import s
+import json
 
 #uses API to get calorie information
 query = s
 api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
 response = requests.get(api_url, headers={'X-Api-Key': 'V9yArwyqPfnqYsxWWwRSng==HTX0HgBcu8SqUeph'})
+portionsize = 2
+
 
 # Check response status
 if response.status_code == requests.codes.ok:
-    print(response.text)
+
+    # Load response data
+    data = json.loads(response.text)
+
+    # Access the first dictionary in the list
+    first_item = data[0]  
+
+    # Access the calories item
+    calories = first_item.get('calories')
+
+    # Multiplication based on portion size
+    overallcalories = calories * portionsize
+
+    # Printing
+    print("Calories per item: ", calories)
+    print("Overall Calories: ", overallcalories)
 else:
     print("Error:", response.status_code, response.text)
