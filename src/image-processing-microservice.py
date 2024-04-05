a0211e1a831bc7d2a66c75b246e37c096bf48815
emmacelia <emmagarvey345@gmail.com>
2024-04-05 18:39:12 +0100

    Update port

--- src/image-processing-microservice.py
+++ src/image-processing-microservice.py
@@ -27,7 +27,7 @@ app.config.update(
 # MySQL Connection
 db = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT"), 
+    port=int(os.getenv("DB_PORT")), 
     user=os.getenv("DB_USER"),  
     password=os.getenv("DB_PASSWORD"), 
     database=os.getenv("DB_NAME")
