c10d5c566892ef3a632e0850877b707eb78ca893
emmacelia <emmagarvey345@gmail.com>
2024-04-06 19:01:00 +0100

    testing yet again

--- src/flask-server.py
+++ src/flask-server.py
@@ -28,7 +28,7 @@ app.config.update(
     SESSION_COOKIE_SAMESITE='None',
 )
 
-MICROSERVICE_URL = 'https://fyppython-production.up.railway.app/'  
+MICROSERVICE_URL = 'https://fyppython-production.up.railway.app'  
 
 CORS(app, supports_credentials=True)
 IMAGES_DIR = os.path.abspath(".\\src\\Images\\")
@@ -114,12 +114,13 @@ def image_process():
 def image_process_manually():
 
     uid = session.get('uid')
+    print("uid in /image_process_manually: ", uid)
     data = request.get_json()
 
     # Extract food name and portion size from the JSON data
     food_name = data.get('foodName')
     portion_size = data.get('portion')
-    
+    print("data in /image_process_manually: ", data)
     if not data:
         return jsonify({'error': 'No data provided'})
     
