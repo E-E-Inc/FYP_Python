22a4d95e9b3a58e9c96e4dc5774bae975757fc20
emmacelia <emmagarvey345@gmail.com>
2024-04-11 20:02:38 +0100

    potential fixes

--- src/flask-server.py
+++ src/flask-server.py
@@ -31,7 +31,7 @@ app.config.update(
 MICROSERVICE_URL = 'https://fyppython-production.up.railway.app' 
 MAIN_URL = 'https://foodlogix.up.railway.app' 
 
-CORS(app, supports_credentials=True, origins=[MICROSERVICE_URL, MAIN_URL])
+CORS(app, supports_credentials=True)
 IMAGES_DIR = os.path.abspath(".\\src\\Images\\")
 
 # MySQL Connection
